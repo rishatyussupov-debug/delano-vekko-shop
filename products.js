615f19f445aee604533e8de525bcd31943568f3e
rishatyussupov-debug <rishat.yussupov@icloud.com>
2026-09-22 15:21:51 +0600

    Update products.js

--- products.js
+++ products.js
@@ -2,22 +2,86 @@ const products = [
 
   {
       id: 1,
-  brand: "DELANO",
-  name: "Смеситель 9072-1T",
-  article: "9072-1T",
+   brand: "DELANO",
+  name: "Смеситель 9072",
+  article: "9072",
   price: 35000,
   category: "delano",
   height: "35 см",
-  image: "9072-1T1.png",
+  image: "9072.png",
   images: [
-    "9072-1T1.png",
-    "9072-1T3.png",
-    "9072-1T2.png"
+    "9072.png",
+    "9072-1.png"
+
   ]
   },
-
   {
       id: 2,
+  brand: "DELANO",
+  name: "Смеситель 9072-1",
+  article: "9072-1",
+  price: 35000,
+  category: "delano",
+  height: "35 см", 
+  image: "9072-11.png",
+  images: [
+    "9072-11.png",
+    "9072-12.png",
+    "9072-13.png",
+    "9072-14.png"
+  ]
+  }, 
+  {
+      id: 3,
+  brand: "DELANO",
+  name: "Смеситель 9072-Q",
+  article: "9072-Q",
+  price: 35000,
+  category: "delano",
+  height: "35 см", 
+  image: "9072-Q1.png",
+  images: [
+    "9072-Q1.png",
+    "9072-F2.png",
+    "9072-F3.png",
+    "9072-F4.png"
+  ]
+  },
+  {
+      id: 4,
+  brand: "DELANO",
+  name: "Смеситель 9072-1Q",
+  article: "9072-1Q",
+  price: 35000,
+  category: "delano",
+  height: "35 см", 
+  image: "9072-1Q1.png",
+  images: [
+    "9072-1Q1.png",
+    "9072-1Q2.png",
+    "9072-1Q3.png",
+    "9072-1Q4.png"
+  ]
+  },
+    {
+      id: 5,
+  brand: "DELANO",
+  name: "Смеситель 9072-F",
+  article: "9072-F",
+  price: 35000,
+  category: "delano",
+  height: "35 см", 
+  image: "9072-F1.png",
+  images: [
+    "9072-F1.png",
+    "9072-F2.png",
+    "9072-F3.png",
+    "9072-F4.png"
+  ]
+  },
+ 
+  {
+      id: 6,
   brand: "DELANO",
   name: "Смеситель 9072-1F",
   article: "9072-1F",
@@ -33,53 +97,111 @@ const products = [
   ]
   },
 
-  {
-    id: 3,
-    brand: "VEKKO",
-    name: "Смеситель для кухни",
-    article: "V001",
-    price: 10500,
-    category: "vekko",
-    image: "v001.jpg"
-  },
 
   {
-    id: 4,
-  brand: "DELANO",
-  name: "Душевая система",
-  article: "9072",
+    id: 7,
+    brand: "DELANO",
+  name: "Смеситель 9072-L",
+  article: "9072-L",
   price: 35000,
   category: "delano",
-  image: "9072.png",
+  height: "35 см",
+  image: "9072-L1.png",
   images: [
-    "9072.png",
-    "9072-1.png"
+    "9072-L1.png",
+    "9072-L2.png",
+    "9072-L2.png"
   ]
   },
 
 {
-  id: 5,
+  id: 8,
   brand: "DELANO",
-  name: "Смеситель 9072-H",
-  article: "9072H",
+  name: "Смеситель 9072-1L",
+  article: "9072-1L",
   price: 35000,
   category: "delano",
-  image: "9072H-1.png",
+  image: "9072-1L1.png",
   images: [
-    "9072H-1.png",
-    "9072H-2.png",
-    "9072H-3.png",
-    "9072H-4.png"
+    "9072-1L1.png",
+    "9072-1L2.png",
+    "9072-1L3.png",
+    "9072-1L4.png"
   ]
 },
   {
-  id: 6,
+  id: 9,
   brand: "DELANO",
-  name: "Душевая система",
-  article: "VK-9023",
+  name: "Смеситель 9072-LT",
+  article: "9072-LT",
   price: 35000,
-  category: "shower",
-  image: "Vk9023.JPG"
+  category: "delano",
+  image: "9072-LT1.png",
+  images: [
+    "9072-LT1.png",
+    "9072-LT2.png",
+    "9072-LT3.png",
+    "9072-LT4.png"
+  ]
 },
-
+{
+  id: 10,
+  brand: "DELANO",
+  name: "Смеситель 9072-1LT",
+  article: "9072-1LT",
+  price: 35000,
+  category: "delano",
+  image: "9072-1LT1.png",
+  images: [
+    "9072-1LT1.png",
+    "9072-1LT2.png",
+    "9072-1LT3.png",
+    "9072-1LT4.png"
+  ]
+},
+  {
+  id: 11,
+  brand: "DELANO",
+  name: "Смеситель 9072-T",
+  article: "9072-T",
+  price: 35000,
+  category: "delano",
+  image: "9072-T1.png",
+  images: [
+    "9072-T1.png",
+    "9072-T2.png",
+    "9072-T3.png",
+    "9072-T4.png"
+  ]
+},
+    {
+  id: 12,
+  brand: "DELANO",
+  name: "Смеситель 9072-H",
+  article: "9072-H",
+  price: 35000,
+  category: "delano",
+  image: "9072-H1.png",
+  images: [
+    "9072-H1.png",
+    "9072-H2.png",
+    "9072-H3.png",
+    "9072-H4.png"
+  ]
+},
+      {
+  id: 12,
+  brand: "DELANO",
+  name: "Смеситель 9072-1H",
+  article: "9072-H",
+  price: 35000,
+  category: "delano",
+  image: "9072-1H1.png",
+  images: [
+    "9072-1H1.png",
+    "9072-1H2.png",
+    "9072-1H3.png",
+    "9072-1H4.png"
+  ]
+},
 ];
