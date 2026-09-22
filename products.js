b817f4921b2ea45d36f84c6493a77c5cffbb81a5
rishatyussupov-debug <rishat.yussupov@icloud.com>
2026-09-22 14:56:55 +0600

    Create products.js

---
+++ products.js
@@ -0,0 +1,85 @@
+const products = [
+
+  {
+      id: 1,
+  brand: "DELANO",
+  name: "Смеситель 9072-1T",
+  article: "9072-1T",
+  price: 35000,
+  category: "delano",
+  height: "35 см",
+  image: "9072-1T1.png",
+  images: [
+    "9072-1T1.png",
+    "9072-1T3.png",
+    "9072-1T2.png"
+  ]
+  },
+
+  {
+      id: 2,
+  brand: "DELANO",
+  name: "Смеситель 9072-1F",
+  article: "9072-1F",
+  price: 35000,
+  category: "delano",
+  height: "35 см", 
+  image: "9072-1F1.png",
+  images: [
+    "9072-1F1.png",
+    "9072-1F2.png",
+    "9072-1F3.png",
+    "9072-1F4.png"
+  ]
+  },
+
+  {
+    id: 3,
+    brand: "VEKKO",
+    name: "Смеситель для кухни",
+    article: "V001",
+    price: 10500,
+    category: "vekko",
+    image: "v001.jpg"
+  },
+
+  {
+    id: 4,
+  brand: "DELANO",
+  name: "Душевая система",
+  article: "9072",
+  price: 35000,
+  category: "delano",
+  image: "9072.png",
+  images: [
+    "9072.png",
+    "9072-1.png"
+  ]
+  },
+
+{
+  id: 5,
+  brand: "DELANO",
+  name: "Смеситель 9072-H",
+  article: "9072H",
+  price: 35000,
+  category: "delano",
+  image: "9072H-1.png",
+  images: [
+    "9072H-1.png",
+    "9072H-2.png",
+    "9072H-3.png",
+    "9072H-4.png"
+  ]
+},
+  {
+  id: 6,
+  brand: "DELANO",
+  name: "Душевая система",
+  article: "VK-9023",
+  price: 35000,
+  category: "shower",
+  image: "Vk9023.JPG"
+},
+
+];
